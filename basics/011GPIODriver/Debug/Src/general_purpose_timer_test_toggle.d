9a746f1cb117b5154eb471b336a9683f3b6c7770
naveen <naveen.pendyala@gmail.com>
2023-12-03 17:08:30 +0530

    Added Timer Code for Toggle Mode

--- basics/011GPIODriver/Debug/Src/general_purpose_timer_test_toggle.d
+++ basics/011GPIODriver/Debug/Src/general_purpose_timer_test_toggle.d
@@ -1,4 +1,5 @@
-Src/general_purpose_timer_test_1.o: ../Src/general_purpose_timer_test_1.c \
+Src/general_purpose_timer_test_toggle.o: \
+ ../Src/general_purpose_timer_test_toggle.c \
  /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/stm32f4xx.h \
  /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/gpio_driver.h \
  /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/stm32f4xx.h \
--- basics/011GPIODriver/Debug/Src/subdir.mk
+++ basics/011GPIODriver/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/general_purpose_timer_test_1.c \
+../Src/general_purpose_timer_test_toggle.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/general_purpose_timer_test_1.o \
+./Src/general_purpose_timer_test_toggle.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/general_purpose_timer_test_1.d \
+./Src/general_purpose_timer_test_toggle.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/general_purpose_timer_test_1.d ./Src/general_purpose_timer_test_1.o ./Src/general_purpose_timer_test_1.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/general_purpose_timer_test_toggle.d ./Src/general_purpose_timer_test_toggle.o ./Src/general_purpose_timer_test_toggle.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
