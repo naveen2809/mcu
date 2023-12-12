9bbe591ff9e1fd2ef3ffc2116d1f0ed13b5c4c75
naveen <naveen.pendyala@gmail.com>
2023-12-12 09:25:08 +0530

    Seven Segment Display Driver (Common Anode)

--- basics/011GPIODriver/Debug/Src/seven_segment_display.d
+++ basics/011GPIODriver/Debug/Src/seven_segment_display.d
@@ -1,4 +1,4 @@
-Src/led_train.o: ../Src/led_train.c \
+Src/seven_segment_display.o: ../Src/seven_segment_display.c \
  /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/timer_driver.h \
  /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/stm32f4xx.h \
  /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/gpio_driver.h
--- basics/011GPIODriver/Debug/Src/subdir.mk
+++ basics/011GPIODriver/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/led_train.c \
+../Src/seven_segment_display.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/led_train.o \
+./Src/seven_segment_display.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/led_train.d \
+./Src/seven_segment_display.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/led_train.d ./Src/led_train.o ./Src/led_train.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/seven_segment_display.d ./Src/seven_segment_display.o ./Src/seven_segment_display.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
