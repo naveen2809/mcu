92ce8809a267702c73a9e705ad8f2c0644f4f94e
naveen <naveen.pendyala@gmail.com>
2023-12-14 19:08:15 +0530

    Updated the Timer Input Capture Code in 'general_purpose_timer_test_input_capture.c' file

--- basics/011GPIODriver/Src/general_purpose_timer_test_input_capture.c
+++ basics/011GPIODriver/Src/general_purpose_timer_test_input_capture.c
@@ -69,19 +69,9 @@
 
 #define TIM2_IRQ_NUM					28
 
-#define UART_TX_PIN						GPIO_PIN_2
-#define UART_RX_PIN						GPIO_PIN_3
-
-
-void configure_uart(void);
-
-char message[] = "Starting capture\r\n";
-char message2[] = "Capture completed\r\n";
-
 uint32_t capture_value[10];
 uint32_t interrupt_count = 0;
 
-struct USART_Handle_t Test_USART;
 struct General_Purpose_Timer_Handle_t TIM2_Handle;
 
 void delay(void)
@@ -105,14 +95,13 @@ void TIM2_IRQHandler(void)
 
 int main(void)
 {
-
 	uint32_t *pRCC_CR = (uint32_t *) RCC_CR_REG_ADDR;
 	uint32_t *pRCC_CFGR = (uint32_t *) RCC_CFGR_REG_ADDR;
 	uint32_t *pRCC_PLLCFGR = (uint32_t *) RCC_PLLCFGR_REG_ADDR;
 	uint32_t *pFLASH_ACR = (uint32_t *) FLASH_ACR_REG_ADDR;
 
-	uint8_t MCO1_SOURCE = MCO1_SOURCE_HSE;
-	uint8_t MCO1_PRESCALAR = MCO_PRESCALAR_4;
+	uint8_t MCO1_SOURCE = MCO1_SOURCE_PLL;
+	uint8_t MCO1_PRESCALAR = MCO_PRESCALAR_5;
 	uint8_t MCO2_SOURCE = MCO2_SOURCE_HSE;
 	uint8_t MCO2_PRESCALAR = MCO_PRESCALAR_4;
 
@@ -121,15 +110,14 @@ int main(void)
 
 	uint16_t PLL_M_VALUE = 4;
 	uint16_t PLL_N_VALUE = 72;
-	uint16_t PLL_P_VALUE = 2;
+	uint16_t PLL_P_VALUE = 1;
 	uint16_t PLL_Q_VALUE = 3;
 
-	uint8_t PPRE1_VALUE = 5;
+	uint8_t PPRE1_VALUE = 4;
 	uint8_t PPRE2_VALUE = 4;
 
 	TIM2_Handle.pGeneral_Purpose_Timer = (struct General_Purpose_Timer_RegDef_t *) TIM2;
 
-	/*
 	//Configure GPIO Pins for MCO1
 	EnablePeriClk(GPIOA);
 	GPIOSetMode(GPIOA,MCO1_GPIO_PIN, GPIO_MODE_ALTFN);
@@ -151,7 +139,6 @@ int main(void)
 	*pRCC_CFGR |= (MCO2_SOURCE << MCO2_SOURCE_BITPOS);
 	*pRCC_CFGR &= ~(0x7 << MCO2_PRESCALAR_BITPOS);
 	*pRCC_CFGR |= (MCO2_PRESCALAR << MCO2_PRESCALAR_BITPOS);
-	*/
 
 	//Configuring the HSE Oscillator
 	*pRCC_CR |= (1 << 16);					//Turning on the HSE Oscillator
@@ -183,8 +170,6 @@ int main(void)
 	*pRCC_CR |= (1 << PLL_ON_BITPOS);					//Turning on the PLL Oscillator
 	while(!(*pRCC_CR & (1 << PLL_CHECK_BITPOS)));		//Checking whether PLL Oscillator is turned on and is stable
 
-
-	/*
 	//Changing the FLASH Latency
 	*pFLASH_ACR |= 0x2;						//Changing the FLASH Latency
 	while(!(*pFLASH_ACR & 0x2));
@@ -195,11 +180,6 @@ int main(void)
 	//Changing the APB1 and APB2 Pre-Scalers
 	*pRCC_CFGR |= (PPRE1_VALUE << RCC_CFGR_PPRE1_BITPOS);
 	*pRCC_CFGR |= (PPRE2_VALUE << RCC_CFGR_PPRE2_BITPOS);
-
-*/
-	//Configuring the UART
-	//configure_uart();
-	//USART_SendData(&Test_USART, (uint8_t *)&message, strlen(message));
 
 	//Timer Configuration
 	memset(&TIM2_Handle.General_Purpose_Timer_Config,0,sizeof(TIM2_Handle.General_Purpose_Timer_Config));
@@ -234,34 +214,4 @@ int main(void)
 	}
 
 	return 0;
-}
-
-
-void configure_uart(void)
-{
-	//GPIO Pin Configuration
-	EnablePeriClk(GPIOA);
-	GPIOSetMode(GPIOA,UART_TX_PIN,GPIO_MODE_ALTFN);
-	GPIOSetMode(GPIOA,UART_RX_PIN,GPIO_MODE_ALTFN);
-	GPIOSetAltFn(GPIOA,UART_TX_PIN,GPIO_ALTFN_7);
-	GPIOSetAltFn(GPIOA,UART_RX_PIN,GPIO_ALTFN_7);
-	GPIOSetOutputType(GPIOA,UART_TX_PIN,GPIO_OPTYPE_PP);
-	GPIOSetOutputType(GPIOA,UART_RX_PIN,GPIO_OPTYPE_PP);
-	GPIOSetOutputSpeed(GPIOA,UART_TX_PIN,GPIO_OPSPEED_HIGH);
-	GPIOSetOutputSpeed(GPIOA,UART_RX_PIN,GPIO_OPSPEED_HIGH);
-	GPIOSetPullUpDownConfig(GPIOA,UART_TX_PIN,GPIO_PULL_UP);
-	GPIOSetPullUpDownConfig(GPIOA,UART_RX_PIN,GPIO_PULL_UP);
-
-	//USART Configuration
-	Test_USART.pUSART = (struct USART_RegDef_t *) USART2;
-	Test_USART.USART_Config.USART_Mode = USART_MODE_TX_RX;
-	Test_USART.USART_Config.USART_DataLength = USART_DATA_LEN_8_BITS;
-	Test_USART.USART_Config.USART_StopBits = USART_STOP_BITS_1;
-	Test_USART.USART_Config.USART_ParityControl = USART_PARITY_DISABLE;
-	Test_USART.USART_Config.USART_BaudRate = USART_SB_RATE_9600;
-	Test_USART.USART_Config.USART_HWFlowControl = USART_HW_FLOW_CNTRL_NONE;
-
-	EnablePeriClk(USART2);
-	USART_Init(&Test_USART);
-	USART_PeripheralEnable(&Test_USART);
 }
