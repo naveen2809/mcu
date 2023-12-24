b66d24b1db025e6a5b3424e1332af32975b70cbf
naveen <naveen.pendyala@gmail.com>
2023-12-24 19:40:37 +0530

    Added code for sending DHT11 Sensor Data over nRFL01 Wireless Link and printing on LCD at other end

---
+++ basics/011GPIODriver/Debug/Driver/Src/common_utils.d
@@ -0,0 +1,21 @@
+Driver/Src/common_utils.o: ../Driver/Src/common_utils.c \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/common_utils.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/stm32f4xx.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/gpio_driver.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/i2c_driver.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/spi_driver.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/usart_driver.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/timer_driver.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/general_purpose_timer.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/lcd_driver_pcf8574.h \
+ /home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/common_utilities.h
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/common_utils.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/stm32f4xx.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/gpio_driver.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/i2c_driver.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/spi_driver.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/usart_driver.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/timer_driver.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/general_purpose_timer.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/lcd_driver_pcf8574.h:
+/home/naveen/Code/mcu/basics/011GPIODriver/Driver/Inc/common_utilities.h:
--- basics/011GPIODriver/Debug/sources.mk
+++ basics/011GPIODriver/Debug/sources.mk
@@ -21,7 +21,6 @@ C_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-Driver/Inc \
 Driver/Src \
 Src \
 Startup \
